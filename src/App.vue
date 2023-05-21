2f5afe2b72288631bfdd19ad81a25b67f5bb0071
shawn <shawnmctc@gmail.com>
2023-05-21 19:27:19 -0300

    Name h1 and href

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Header from "@/components/Header.vue";
 </script>
 
 <template>
-    <div class="flex flex-col min-h-screen bg-black">
+    <div class="flex flex-col min-h-screen bg-gray-800">
         <Header />
 
     </div>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,9 +4,14 @@ import BxlGithub from "@/components/icons/BxlGithub.vue";
 
 <template>
     <header>
-        <nav class="flex items-center justify-between bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-            <div class="ml-auto">
-                <BxlGithub class="w-10 h-12" />
+        <nav class="flex items-center justify-between border-gray-200 px-4 lg:px-6 py-1 dark:bg-gray-800">
+            <div>
+                <h1 class="text-2xl font-bold text-white opacity-50 pl-60">
+                    Shawn Nectar
+                </h1>
+            </div>
+            <div class="pr-60">
+                <BxlGithub />
             </div>
         </nav>
     </header>
