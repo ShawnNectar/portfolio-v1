492d9b008a2936dd47af1485c4a13b64f3241f4b
shawn <shawnmctc@gmail.com>
2023-05-21 18:38:43 -0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import './index.css'
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+    <div class="flex flex-col min-h-screen bg-black">
+        <Header />
+
+    </div>
+</template>
+
+
