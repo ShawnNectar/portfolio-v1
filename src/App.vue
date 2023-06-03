022f82ddc50bc2bac738e0ef142fbcb9b65c8248
shawn <shawnmctc@gmail.com>
2023-06-03 17:17:46 -0300

    [new model] print formatting and distri grid

--- src/App.vue
+++ src/App.vue
@@ -3,88 +3,93 @@ import './index.css'
 </script>
 
 <template>
-    <h1>Hey, I'm Shawn!</h1>
-    <div>
-        <div class="text-4xl text-blue-200">
-            <h1 class="text-green-600">// Stuff I do: </h1>
-            <h2>
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"World-improving tools 🛠️"</span>);
-            </h2>
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Things to make life better💡"</span>);
-            <h2>
+    <div class="px-10 pt-6">
+        <h1 class="text-5xl font-semibold pb-5 text-amber-200">👋 Hey, I'm Shawn!</h1>
+        <div class="grid grid-cols-2">
+            <div class="text-2xl text-blue-200 font-mono">
+                <h1 class="text-green-600 py-5">// Stuff I do: </h1>
+                <h2 class="pb-2">
+                    <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"World-improving tools 🛠️"</span>);
+                </h2 >
+                <h2 class="pb-2">
+                    <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Things to make life better💡"</span>);
+                </h2>
+                <h2 class="pb-2">
+                    <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Talk a lot 🗣️"</span>);
+                </h2>
+                <h2 class="pb-2">
+                    <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Study tech all day 📖"</span>);
+                </h2>
+                <h2 class="pb-3">
+                    <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Have some fun with biology 🧬"</span>);
+                </h2>
+            </div>
 
+            <div class="text-2xl text-blue-200 font-mono">
+                <h1 class="text-green-600 py-5">// About me: </h1>
+                <h2 class="pb-2">
+                    <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"17yo who lives in Brazil 🇧🇷"</span>);
+                </h2>
+                <h2 class="pb-2">
+                    <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Junior developer 👨‍💻"</span>);
+                </h2>
+                <h2 class="pb-2">
+                    <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Love math ♾️"</span>);
+                </h2>
+                <h2 class="pb-2">
+                    <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Passionated about tech 🌐"</span>);
+                </h2>
+                <h2 class="pb-3">
+                    <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Want to explore the universe 🔭"</span>);
+                </h2>
+            </div>
+        </div>
+
+        <div class="text-2xl text-blue-200 font-mono">
+            <h1 class="text-green-600 py-5">// Work interests: </h1>
+            <h2 class="pb-2">
+                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Math development 🔢 @ specially algorithms"</span>);
             </h2>
-            <h2>
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Talk a lot 🗣️"</span>);
+            <h2 class="pb-2">
+                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"IoT technology 📡 @ I've been working on house automation"</span>);
             </h2>
-            <h2>
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Study tech all day 📖"</span>);
+            <h2 class="pb-2">
+                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Robotics 🤖 @ I love anything related to robotics LOVE IT"</span>);
+            </h2>
+            <h2 class="pb-2">
+                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Machine Learning 🧠 @ no further explanation, it's gold"</span>);
+            </h2>
+            <h2 class="pb-2">
+                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Real-life applied stuff 🌐 @ why would we build useless stuff?"</span>);
             </h2>
         </div>
 
-        <div class="text-4xl text-blue-200">
-            <h1 class="text-green-600">// About me: </h1>
-            <h2>
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"17yo who lives in Brazil 🇧🇷"</span>);
-            </h2>
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Junior developer 👨‍💻"</span>);
-            <h2>
-
-            </h2>
-            <h2>
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Love math ♾️"</span>);
-            </h2>
-            <h2>
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Passionated about tech 🌐"</span>);
-            </h2>
-
-            <h2>
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Want to explore the universe 🔭"</span>);
-            </h2>
         </div>
-        <div class="text-4xl text-blue-200">
-            <h1 class="text-green-600">// Work interests: </h1>
-            <h2>
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Math development 🔢"</span>);
-            </h2>
-            <h2>
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"IoT technology 📡"</span>);
-            </h2>
-            <h2>
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Robotics 🤖"</span>);
-            </h2>
-            <h2>
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Machine Learning 🧠"</span>);
-            </h2>
-            <h2>
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Real-life applied stuff 🌐"</span>);
-            </h2>
-        </div>
-
-    </div>
-    <div>
-        <h1>let github = String::from("github");</h1>
         <div>
-            <h1>let email = String::from("shawnnectar@protonmail.com");</h1>
-            <h1>let discord = String::from("ShawnNectar#8155");</h1>
+            <h1>let github = String::from("github");</h1>
+            <div>
+                <h1>let email = String::from("shawnnectar@protonmail.com");</h1>
+                <h1>let discord = String::from("ShawnNectar#8155");</h1>
+            </div>
         </div>
-    </div>
-    <div><h1>let brazil = String::from("<br/>⠀⠀⠀⠀⠀⠀⠀⠀⠀
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
-        ⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣆⠀⣀⣀⣤⣴⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
-        ⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
-        ⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⣀⡀⠀⠀⠀⠀<br/>
-        ⠀⢠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡄⠀<br/>
-        ⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀<br/>
-        ⠀⠀⠈⠛⠉⠁⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀<br/>
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀<br/>
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀<br/>
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀<br/>
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀<br/>
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⡟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
-        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");</h1></div>
+        <div>
+            <h1>let brazil = String::from("<br/>⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+            ⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣆⠀⣀⣀⣤⣴⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+            ⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+            ⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⣀⡀⠀⠀⠀⠀<br/>
+            ⠀⢠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡄⠀<br/>
+            ⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀<br/>
+            ⠀⠀⠈⠛⠉⠁⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀<br/>
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀<br/>
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀<br/>
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀<br/>
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀<br/>
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⡟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br/>
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");</h1>
+        </div>
 </template>
 
 
