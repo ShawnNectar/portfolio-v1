eb7200efd81f2de18c61a6d9712d47506ad0181b
shawn <shawnmctc@gmail.com>
2023-06-03 20:11:10 -0300

    [Deploy] deploy

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/portfolio-v1/",
+  base: "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
