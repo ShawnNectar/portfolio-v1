644b4954f844f92b2567699109c79b0e1cccf1c2
shawn <shawnmctc@gmail.com>
2023-06-03 19:14:04 -0300

    deploy

--- src/components/Connections.vue
+++ src/components/Connections.vue
@@ -6,7 +6,7 @@
     <div class="font-mono text-2xl text-blue-200 pt-10 pb-5">
         <h1 class="text-2xl text-green-600 font-mono pb-2">// Connections: </h1>
         <span class="text-orange-500">let </span>github = String::<span style="color: #50a8e4;">from</span>(<span
-            style="color: #e70d0d;">"<a style="text-decoration: underline" href="https://github.com/ShawnNectar">github</a>"</span>);
+            style="color: #e70d0d;">"<a style="text-decoration: underline" href="https://github.com/ShawnNectar" target=”_blank”>github</a>"</span>);
         <div>
             <h1 class="pt-6 pb-2">
                 <span class="text-orange-500">let </span>email = String::<span
