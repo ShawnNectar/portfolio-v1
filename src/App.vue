2be37f168785f11d42c76c26e8eec2fa67df33cd
shawn <shawnmctc@gmail.com>
2023-06-03 21:39:52 -0300

    [New Format] selection color + cols fix

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,17 @@ import Brazil from "@/components/Brazil.vue";
     <div class="lg:px-10 md:px-10 sm:px-0 pt-6">
         <div class="lg:px-0 md:px-10 sm:px-10">
             <h1 class="text-5xl font-semibold pb-5 text-amber-200">🤗 Hey, I'm Shawn!</h1>
-            <div class="grid 2xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1">
+            <div class="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1">
                 <StuffIDo />
                 <AboutMe />
-                <div class="md:col-span-2">
-                    <WorkInterests />
-                </div>
             </div>
-        </div>
 
-        <Connections />
-        <Brazil />
+            <WorkInterests />
+
+
+            <Connections />
+            <Brazil />
+        </div>
     </div>
 </template>
 
