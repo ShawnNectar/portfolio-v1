978849cc492fab3e58d55dd6d1397673849070b4
shawn <shawnmctc@gmail.com>
2023-06-03 21:11:38 -0300

    [minor] work -> working

--- src/components/WorkInterests.vue
+++ src/components/WorkInterests.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div class="text-2xl text-blue-200 font-mono">
-        <h1 class="text-green-600 py-5">// Work interests </h1>
+        <h1 class="text-green-600 py-5">// Working interests </h1>
         <h2 class="pb-2">
             <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Math development 🔢 ## specially algorithms"</span>);
         </h2>
