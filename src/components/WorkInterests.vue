c2d220f5adecda925953f07721c5e4dd65ea6ce0
shawn <shawnmctc@gmail.com>
2023-06-04 23:43:14 -0300

    [Minor] 4 test mobile prodc

--- src/components/WorkInterests.vue
+++ src/components/WorkInterests.vue
@@ -6,24 +6,19 @@
     <div class="text-lg md:text-2xl text-blue-200 font-mono">
         <h1 class="text-green-600 py-2 md:py-5">// Working interests</h1>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>
-            (<span style="color: #e70d0d;">"Math development 🔢 ## specially algorithms"</span>);
+            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Math development 🔢 ## specially algorithms"</span>);
         </h2>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>
-            (<span style="color: #e70d0d;">"IoT technology 📡 ## I've been working on home automation"</span>);
+            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"IoT technology 📡 ## I've been working on home automation"</span>);
         </h2>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>
-            (<span style="color: #e70d0d;">"Robotics 🤖 ## I love anything related to robotics LOVE IT"</span>);
+            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Robotics 🤖 ## I love anything related to robotics LOVE IT"</span>);
         </h2>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>
-            (<span style="color: #e70d0d;">"Machine Learning 🧠 ## no further explanation, it's gold"</span>);
+            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Machine Learning 🧠 ## no further explanation, it's gold"</span>);
         </h2>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>
-            (<span style="color: #e70d0d;">"Real-life applied stuff 🌐 ## why would we build useless stuff?"</span>);
+            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Real-life applied stuff 🌐 ## why would we build useless stuff?"</span>);
         </h2>
     </div>
 </template>
