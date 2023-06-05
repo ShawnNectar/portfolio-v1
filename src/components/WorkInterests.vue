04afea1013212e3f61e111e7aefefad9b180f5f3
shawn <shawnmctc@gmail.com>
2023-06-05 00:59:51 -0300

    [Minor] WI break-line when mob screen to fit

--- src/components/WorkInterests.vue
+++ src/components/WorkInterests.vue
@@ -1,29 +1,31 @@
 <script setup>
-
 </script>
 
 <template>
     <div class="text-sm md:text-2xl text-blue-200 font-mono">
         <h1 class="text-green-600 py-2 md:py-5">// Working interests</h1>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Math development 🔢 ## specially algorithms"</span>);
+            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Math development 🔢 <br class="md:hidden"/>## specially algorithms"</span>);
         </h2>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"IoT technology 📡 ## I've been working on home automation"</span>);
+            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"IoT technology 📡 <br class="md:hidden"/>## I've been working on home automation"</span>);
         </h2>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Robotics 🤖 ## I love anything related to robotics LOVE IT"</span>);
+            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Robotics 🤖 <br class="md:hidden"/>## I love anything related to robotics LOVE IT"</span>);
         </h2>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Machine Learning 🧠 ## no further explanation, it's gold"</span>);
+            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Machine Learning 🧠 <br class="md:hidden"/>## no further explanation, it's gold"</span>);
         </h2>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Real-life applied stuff 🌐 ## why would we build useless stuff?"</span>);
+            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Real-life applied stuff 🌐 <br class="md:hidden"/>## why would we build useless stuff?"</span>);
         </h2>
-
     </div>
 </template>
 
 <style scoped>
-
+@media (min-width: 768px) {
+    .md\:hidden {
+        display: none;
+    }
+}
 </style>
