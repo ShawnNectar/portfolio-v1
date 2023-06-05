7f1122b81ca97cb2d51b8df9268b61294d7129fa
shawn <shawnmctc@gmail.com>
2023-06-04 22:08:14 -0300

    [Minor] StuffIDo test mobile prodc

--- src/components/StuffIDo.vue
+++ src/components/StuffIDo.vue
@@ -5,23 +5,29 @@
 <template>
     <div class="text-2xl text-blue-200 font-mono">
         <h1 class="text-green-600 py-5">// Stuff I do </h1>
-        <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"World-improving tools 🛠️"</span>);
+        <h2 class="pb-2" style="white-space: nowrap;">
+            <span class="text-teal-600">println!</span>
+            (<span style="color: #e70d0d;">"World-improving tools 🛠️"</span>);
         </h2>
-        <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Things to make life better 💡"</span>);
+        <h2 class="pb-2" style="white-space: nowrap;">
+            <span class="text-teal-600">println!</span>
+            (<span style="color: #e70d0d;">"Things to make life better 💡"</span>);
         </h2>
-        <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Talk a lot 🗣️"</span>);
+        <h2 class="pb-2" style="white-space: nowrap;">
+            <span class="text-teal-600">println!</span>
+            (<span style="color: #e70d0d;">"Talk a lot 🗣️"</span>);
         </h2>
-        <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Study tech all day 📖"</span>);
+        <h2 class="pb-2" style="white-space: nowrap;">
+            <span class="text-teal-600">println!</span>
+            (<span style="color: #e70d0d;">"Study tech all day 📖"</span>);
         </h2>
-        <h2 class="pb-3">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Have some fun with biology 🧬"</span>);
+        <h2 class="pb-3" style="white-space: nowrap;">
+            <span class="text-teal-600">println!</span>
+            (<span style="color: #e70d0d;">"Have some fun with biology 🧬"</span>);
         </h2>
     </div>
 </template>
+
 
 <style scoped>
 
