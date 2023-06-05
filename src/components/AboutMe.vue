79b834b30f525d8dd0c6b4601ce7e8dbad12a0ed
shawn <shawnmctc@gmail.com>
2023-06-04 21:50:50 -0300

    [Minor] readjust

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div class="text-2xl text-blue-200 font-mono">
-        <h1 class="text-green-600 py-5">// About me</h1>
+        <h1 class="text-green-600 py-5">// About me </h1>
         <h2 class="pb-2">
             <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"17yo who lives in Brazil 🇧🇷"</span>);
         </h2>
@@ -15,7 +15,7 @@
             <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Love math ♾️"</span>);
         </h2>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Passionate about tech 🌐"</span>);
+            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Passionated about tech 🌐"</span>);
         </h2>
         <h2 class="pb-3">
             <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Want to explore the universe 🔭"</span>);
