f43993a355c098dd7e7c9c323e0a3238fdc08f12
shawn <shawnmctc@gmail.com>
2023-06-04 22:47:13 -0300

    [Minor] STI test mobile prodc

--- src/components/StuffIDo.vue
+++ src/components/StuffIDo.vue
@@ -6,24 +6,30 @@
     <div class="text-lg md:text-2xl text-blue-200 font-mono">
         <h1 class="text-green-600 py-2 md:py-5">// Stuff I do</h1>
         <ul class="list-none pl-0">
-            <li class="pb-2">
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"World-improving tools 🛠️"</span>);
+            <li class="pb-2 whitespace-nowrap">
+                <span class="text-teal-600">println!</span>
+                (<span style="color: #e70d0d;" class="text-base md:text-lg">"World-improving tools 🛠️"</span>);
             </li>
-            <li class="pb-2">
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Things to make life better 💡"</span>);
+            <li class="pb-2 whitespace-nowrap">
+                <span class="text-teal-600">println!</span>
+                (<span style="color: #e70d0d;" class="text-base md:text-lg">"Things to make life better 💡"</span>);
             </li>
-            <li class="pb-2">
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Talk a lot 🗣️"</span>);
+            <li class="pb-2 whitespace-nowrap">
+                <span class="text-teal-600">println!</span>
+                (<span style="color: #e70d0d;" class="text-base md:text-lg">"Talk a lot 🗣️"</span>);
             </li>
-            <li class="pb-2">
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Study tech all day 📖"</span>);
+            <li class="pb-2 whitespace-nowrap">
+                <span class="text-teal-600">println!</span>
+                (<span style="color: #e70d0d;" class="text-base md:text-lg">"Study tech all day 📖"</span>);
             </li>
-            <li class="pb-3">
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Have some fun with biology 🧬"</span>);
+            <li class="pb-3 whitespace-nowrap">
+                <span class="text-teal-600">println!</span>
+                (<span style="color: #e70d0d;" class="text-base md:text-lg">"Have some fun with biology 🧬"</span>);
             </li>
         </ul>
     </div>
 </template>
+
 
 
 
