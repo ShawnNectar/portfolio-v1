835e2e108f3d723f40013bd351f6c6ee8b222b83
shawn <shawnmctc@gmail.com>
2023-06-05 20:37:46 -0300

    [deploy] [minor] theme change

--- src/components/StuffIDo.vue
+++ src/components/StuffIDo.vue
@@ -7,19 +7,19 @@
         <h1 class="text-green-600 py-2 md:py-5 ">// Stuff I do</h1>
         <ul class="list-none pl-0">
             <li class="pb-2">
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"World-improving tools 🛠️"</span>);
+                <span style="color:#79c7ba">println!</span>(<span style="color: #fc8140;">"World-improving tools 🛠️"</span>);
             </li>
             <li class="pb-2">
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Things to make life better 💡"</span>);
+                <span style="color:#79c7ba">println!</span>(<span style="color: #fc8140;">"Things to make life better 💡"</span>);
             </li>
             <li class="pb-2">
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Talk a lot 🗣️"</span>);
+                <span style="color:#79c7ba">println!</span>(<span style="color: #fc8140;">"Talk a lot 🗣️"</span>);
             </li>
             <li class="pb-2">
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Study tech all day 📖"</span>);
+                <span style="color:#79c7ba">println!</span>(<span style="color: #fc8140;">"Study tech all day 📖"</span>);
             </li>
             <li class="pb-3">
-                <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Have some fun with biology 🧬"</span>);
+                <span style="color:#79c7ba">println!</span>(<span style="color: #fc8140;">"Have some fun with biology 🧬"</span>);
             </li>
         </ul>
     </div>
--- src/components/WorkInterests.vue
+++ src/components/WorkInterests.vue
@@ -5,19 +5,19 @@
     <div class="text-sm sm:text-3xl md:text-2xl text-blue-200 font-mono">
         <h1 class="text-green-600 py-2 md:py-5 ">// Working interests</h1>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Math development 🔢 <br class="md:hidden"/>## specially algorithms"</span>);
+            <span style="color:#79c7ba">println!</span>(<span style="color: #fc8140;">"Math development 🔢 <br class="md:hidden"/>## specially algorithms"</span>);
         </h2>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"IoT technology 📡 <br class="md:hidden"/>## I've been working on home automation"</span>);
+            <span style="color:#79c7ba">println!</span>(<span style="color: #fc8140;">"IoT technology 📡 <br class="md:hidden"/>## I've been working on home automation"</span>);
         </h2>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Robotics 🤖 <br class="md:hidden"/>## I love anything related to robotics LOVE IT"</span>);
+            <span style="color:#79c7ba">println!</span>(<span style="color: #fc8140;">"Robotics 🤖 <br class="md:hidden"/>## I love anything related to robotics LOVE IT"</span>);
         </h2>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Machine Learning 🧠 <br class="md:hidden"/>## no further explanation, it's gold"</span>);
+            <span style="color:#79c7ba">println!</span>(<span style="color: #fc8140;">"Machine Learning 🧠 <br class="md:hidden"/>## no further explanation, it's gold"</span>);
         </h2>
         <h2 class="pb-2">
-            <span class="text-teal-600">println!</span>(<span style="color: #e70d0d;">"Real-life applied stuff 🌐 <br class="md:hidden"/>## why would we build useless stuff?"</span>);
+            <span style="color:#79c7ba">println!</span>(<span style="color: #fc8140;">"Real-life applied stuff 🌐 <br class="md:hidden"/>## why would we build useless stuff?"</span>);
         </h2>
     </div>
 </template>
