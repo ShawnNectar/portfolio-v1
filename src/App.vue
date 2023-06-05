7aedd8f288a48031928e4dbe5274c9272be1d3b9
shawn <shawnmctc@gmail.com>
2023-06-04 21:46:59 -0300

    [Deploy] mobile readjust v1

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,21 @@ import Brazil from "@/components/Brazil.vue";
     <div class="lg:px-10 md:px-10 sm:px-0 pt-6">
         <div class="lg:px-0 md:px-10 sm:px-10">
             <h1 class="text-5xl font-semibold pb-5 text-amber-200">🤗 Hey, I'm Shawn!</h1>
-            <div class="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1">
-                <StuffIDo />
-                <AboutMe />
+            <div class="lg:flex md:block sm:block">
+                <div class="lg:w-1/2 md:w-full sm:w-full">
+                    <StuffIDo />
+                </div>
+                <div class="lg:w-1/2 md:w-full sm:w-full">
+                    <AboutMe />
+                </div>
             </div>
-
             <WorkInterests />
-
-
             <Connections />
             <Brazil />
         </div>
     </div>
 </template>
+
 
 
 
--- src/components/Connections.vue
+++ src/components/Connections.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="font-mono text-2xl text-blue-200 pt-10 pb-5">
-        <h1 class="text-2xl text-green-600 font-mono pb-2">// Connections </h1>
+        <h1 class="text-2xl text-green-600 font-mono pb-2">// Connections</h1>
         <div>
             <h1 class="pt-6 pb-2">
-                <span class="text-orange-500">let </span>github = String::<span style="color: #50a8e4;">from</span>(<span
-                style="color: #e70d0d;">"<a style="text-decoration: underline" href="https://github.com/ShawnNectar" target=”_blank”>github</a>"</span>);
+                <span class="text-orange-500">let</span> github = String::<span style="color: #50a8e4;">from</span>(<span style="color: #e70d0d;">"<a style="text-decoration: underline" href="https://github.com/ShawnNectar" target="_blank">github</a>"</span>);
             </h1>
             <h1 class="pt-6 pb-2">
-                <span class="text-orange-500">let </span>email = String::<span style="color: #50a8e4;">from</span>(<span style="color: #e70d0d; cursor: pointer;" @click="copyText('shawnnectar@protonmail.com', 'showCopiedEmail')" ref="emailRef">"shawnnectar@protonmail.com"</span>);
-                <span v-if="showCopiedEmail" class="text-green-500 ml-2">(Copied!)</span>
+                <span class="text-orange-500">let</span> email = String::<span style="color: #e70d0d;">"<a style="text-decoration: underline" href="mailto:shawn@example.com">shawn@example.com</a>"</span>);
             </h1>
-            <h1 class="pb-2">
-                <span class="text-orange-500">let </span>discord = String::<span style="color: #50a8e4;">from</span>(<span style="color: #e70d0d; cursor: pointer;" @click="copyText('ShawnNectar#8155', 'showCopiedDiscord')" ref="discordRef">"ShawnNectar#8155"</span>);
-                <span v-if="showCopiedDiscord" class="text-green-500 ml-2">(Copied!)</span>
+            <h1 class="pt-6 pb-2">
+                <span class="text-orange-500">let</span> linkedin = String::<span style="color: #e70d0d;">"<a style="text-decoration: underline" href="https://linkedin.com/in/shawnnectar" target="_blank">linkedin</a>"</span>);
+            </h1>
+            <h1 class="pt-6 pb-2">
+                <span class="text-orange-500">let</span> twitter = String::<span style="color: #e70d0d;">"<a style="text-decoration: underline" href="https://twitter.com/ShawnNectar" target="_blank">twitter</a>"</span>);
             </h1>
         </div>
     </div>
