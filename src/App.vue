8492a314f0e53e5735323bc46292104d3057c873
shawn <shawnmctc@gmail.com>
2023-06-05 00:43:24 -0300

    [Minor] A test mobile prodc

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import Brazil from "@/components/Brazil.vue";
 </script>
 
 <template>
-    <div class="px-6 py-4 md:px-10">
+    <div class="px-4 py-4 md:px-10">
         <h1 class="text-3xl font-semibold pb-4 md:text-5xl md:pb-5 text-amber-200">🤗 Hey, I'm Shawn!</h1>
         <div class="md:flex md:flex-wrap">
             <div class="md:w-1/2 mb-4">
