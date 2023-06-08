f61da8578683fe10adf39f28cc3baaf5376acc08
shawn <shawnmctc@gmail.com>
2023-06-08 19:22:23 -0300

    [deploy] [minor] brazil fix

--- src/components/Brazil.vue
+++ src/components/Brazil.vue
@@ -14,18 +14,18 @@
             <span style="color: #009B3A">l</span> = String::<span style="color: #50a8e4;">from</span>("<br/>
             <span style='color: #00843D'>⠀⠀⠀⠀⠀⠀⠀⠀⠀</span><br/>
             <span style='color: #0142c7'>⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣆⠀⣀⣀⣤⣴⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</span><br/>
-            <span style='color: #FEDF00'>⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀</span><br/>
+            <span style='color: #FEDF00'>⠀⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀</span><br/>
             <span style='color: #FF6600'>⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⣀⡀⠀⠀⠀⠀</span><br/>
             <span style='color: #009B3A'>⢠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡄⠀</span><br/>
             <span style='color: #00843D'>⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀</span><br/>
             <span style='color: #FFCC29'>⠀⠈⠛⠉⠁⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀</span><br/>
-            <span style='color: #0142c7'>⠀⠀⠀⠀⠀⠀⠀⠉⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀</span><br/>
-            <span style='color: #FEDF00'>⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀</span><br/>
-            <span style='color: #FF6600'>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀</span><br/>
-            <span style='color: #009B3A'>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀</span><br/>
+            <span style='color: #0142c7'>⠀⠀⠀⠀ ⠀⠀⠀⠉⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀</span><br/>
+            <span style='color: #FEDF00'>⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀</span><br/>
+            <span style='color: #FF6600'>⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀</span><br/>
+            <span style='color: #009B3A'>⠀⠀ ⠀⠀⠀⠀⠀⠀⠀   ⢿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀</span><br/>
             <span style='color: #00843D'>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⡟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀</span><br/>
-            <span style='color: #FFCC29'>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</span><br/>
-            <span style='color: #0142c7'>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</span><br/>
+            <span style='color: #FFCC29'>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</span><br/>
+            <span style='color: #0142c7'>⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠙⠻⢿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</span><br/>
             ");
         </h1>
     </div>
